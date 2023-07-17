014342670c05e171ce4ca3beeb7e347c06edbd20
Ben Motmans <ben.motmans@gmail.com>
2023-07-18 01:57:40 +0200

    Pie series options

---
+++ src/Vizor.ECharts/Options/LineCoordinateSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Vizor.ECharts.Options;
+
+/// <summary>
+/// see https://echarts.apache.org/en/option.html#series-line.coordinateSystem
+/// </summary>
+[JsonConverter(typeof(LineCoordinateSystemConverter))]
+public enum LineCoordinateSystem
+{
+	Cartesian2D,
+	Polar
+}
+
+public class LineCoordinateSystemConverter : JsonConverter<LineCoordinateSystem>
+{
+	public override LineCoordinateSystem Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+	{
+		throw new NotImplementedException("Deserialization is not implemented for LineCoordinateSystem.");
+	}
+
+	public override void Write(Utf8JsonWriter writer, LineCoordinateSystem value, JsonSerializerOptions options)
+	{
+		// Serialize the enum value as a lower-case string
+		writer.WriteStringValue(value.ToString().ToLower());
+	}
+}
---
+++ src/Vizor.ECharts/Options/MarkArea.cs
@@ -0,0 +1,6 @@
+﻿namespace Vizor.ECharts.Options;
+
+public class MarkArea
+{
+    //TODO
+}
