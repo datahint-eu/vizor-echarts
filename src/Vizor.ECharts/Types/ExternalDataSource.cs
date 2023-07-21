daf918fb5515e628dc4f24868785241f0efd4dfc
Ben Motmans <ben.motmans@gmail.com>
2023-07-22 01:45:24 +0200

    added TODO

--- src/Vizor.ECharts/Types/ExternalDataSource.cs
+++ src/Vizor.ECharts/Types/ExternalDataSource.cs
@@ -11,6 +11,18 @@ public class ExternalDataSource
 		Url = url;
 	}
 
+	//TODO: support additional fetch parameters, see https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch, https://developer.mozilla.org/en-US/docs/Web/API/fetch
+	//	method: "POST", // *GET, POST, PUT, DELETE, etc.
+	//	mode: "cors", // no-cors, *cors, same-origin
+	//	cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+	//	credentials: "same-origin", // include, *same-origin, omit
+	//	headers: {
+	//	  "Content-Type": "application/json",
+	//	  // 'Content-Type': 'application/x-www-form-urlencoded',
+	//	},
+	//	redirect: "follow", // manual, *follow, error
+	//	referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+
 	public string Url { get; }
 
 	public static explicit operator ExternalDataSource(string url)
