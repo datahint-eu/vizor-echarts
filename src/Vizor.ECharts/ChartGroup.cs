0f1cd6b1190af72bc623c5e3e04f80eec9c4a450
Ben Motmans <ben.motmans@gmail.com>
2023-08-01 13:37:16 +0200

    support for grouping charts

---
+++ src/Vizor.ECharts/ChartGroup.cs
@@ -0,0 +1,27 @@
+﻿namespace Vizor.ECharts;
+
+/// <summary>
+/// Allows multiple charts to be grouped together, allowing easy bulk update calls
+/// </summary>
+public class ChartGroup
+{
+	private List<EChart> charts = new List<EChart>();
+
+	internal void Add(EChart chart)
+	{
+		charts.Add(chart);
+	}
+
+	internal void Remove(EChart chart)
+	{
+		charts.Remove(chart);
+	}
+
+    public async Task UpdateAsync(bool executeDataLoader = true)
+	{
+		foreach (var chart in charts)
+		{
+			await chart.UpdateAsync(executeDataLoader);
+		}
+	}
+}
