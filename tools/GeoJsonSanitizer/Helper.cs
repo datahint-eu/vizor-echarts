47bb6f4363e24213bf25e3c6f529ccfb8b3beb86
Ben Motmans <ben.motmans@gmail.com>
2024-03-12 11:49:20 +0100

    helper tool to sanitize geojson data

---
+++ tools/GeoJsonSanitizer/Helper.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using System.Text.Json.Nodes;
+
+namespace GeoJsonSanitizer;
+
+internal class Helper
+{
+	internal static void RemoveProperties(JsonNode? node)
+	{
+		if (node == null)
+			return;
+
+		if (node is JsonObject obj)
+		{
+			obj.Remove("ID_0");
+			obj.Remove("NAME_0");
+			obj.Remove("ID_1");
+			obj.Remove("NAME_1");
+			obj.Remove("ID_2");
+			obj.Remove("NAME_2");
+			obj.Remove("ID_3");
+			obj.Remove("NAME_3");
+			obj.Remove("ID_4");
+			obj.Remove("NAME_4");
+			obj.Remove("VARNAME_4");
+
+			obj.Remove("ISO");
+			obj.Remove("ENGTYPE_4");
+			obj.Remove("TYPE_4");
+
+			obj.Remove("NAME_2 - New");
+			obj.Remove("NAME_3 - NEW");
+			obj.Remove("NAME_4_NEW");
+
+			foreach (var pair in obj)
+			{
+				RemoveProperties(pair.Value);
+			}
+		}
+		else if (node is JsonArray array)
+		{
+			foreach (var item in array)
+			{
+				RemoveProperties(item);
+			}
+		}
+	}
+}
---
+++ tools/GeoJsonSanitizer/Program.cs
@@ -0,0 +1,34 @@
+﻿using GeoJsonSanitizer;
+using System.Text.Json.Nodes;
+
+if (args.Length != 1)
+{
+	Console.WriteLine("Usage: GeoJsonSanitizer <inputFilePath>");
+	return -1;
+}
+
+string inputFilePath = args[0];
+if (!File.Exists(inputFilePath))
+{
+	Console.WriteLine("ERROR: Input file does not exist.");
+	return -1;
+}
+
+try
+{
+	string jsonContent = File.ReadAllText(inputFilePath);
+	var root = JsonNode.Parse(jsonContent);
+
+	Helper.RemoveProperties(root);
+
+	string modifiedJson = root.ToJsonString();
+	File.WriteAllText(inputFilePath + ".new", modifiedJson);
+
+	Console.WriteLine("Properties removed successfully.");
+	return 0;
+}
+catch (Exception ex)
+{
+	Console.WriteLine("ERROR: " + ex.Message);
+	return -1;
+}
