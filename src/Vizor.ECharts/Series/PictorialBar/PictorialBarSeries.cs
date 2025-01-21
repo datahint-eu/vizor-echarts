3abd1aed6f94d5bfc17e541bc1a851114f226149
Ben Motmans <ben.motmans@gmail.com>
2025-01-21 15:10:39 +0100

    echarts 5.5.0: [pictorialBar] support clip for pictorialBar series

--- src/Vizor.ECharts/Series/PictorialBar/PictorialBarSeries.cs
+++ src/Vizor.ECharts/Series/PictorialBar/PictorialBarSeries.cs
@@ -1022,6 +1022,25 @@ public partial class PictorialBarSeries : ISeries
 	public MarkArea? MarkArea { get; set; }
 
 	/// <summary>
+	/// Since v4.4.0   
+	/// If clip the overflow on the coordinate system.
+	/// Clip results varies between series:   Scatter/EffectScatter：Ignore the symbols exceeds the coordinate system.
+	/// Not clip the elements.
+	///  Bar：Clip all the overflowed.
+	/// With bar width kept.
+	///  Line：Clip the overflowed line.
+	///  Lines: Clip all the overflowed.
+	///  Candlestick: Ignore the elements exceeds the coordinate system.
+	///  Custom: Clip all the olverflowed.
+	///   
+	/// All these series have default value true except pictorialBar and custom series. 
+	/// Set it to false if you don't want to clip.
+	/// </summary>
+	[JsonPropertyName("clip")]
+	[DefaultValue("false")]
+	public bool? Clip { get; set; }
+
+	/// <summary>
 	/// zlevel value of all graphical elements in Pictorial bar chart .
 	///  
 	/// zlevel is used to make layers with Canvas.
