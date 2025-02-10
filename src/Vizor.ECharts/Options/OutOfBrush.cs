1620ed4d800ca58dc24fb18c9f0482a825f005d2
Ben Motmans <ben.motmans@gmail.com>
2025-02-10 09:24:28 +0100

    Added missing properties in OutOfBrush

--- src/Vizor.ECharts/Options/OutOfBrush.cs
+++ src/Vizor.ECharts/Options/OutOfBrush.cs
@@ -1,11 +1,42 @@
-
-using System.ComponentModel;
 using System.Text.Json.Serialization;
 
 namespace Vizor.ECharts;
 
 public partial class OutOfBrush
 {
-    [JsonPropertyName("colorAlpha")]
+	[JsonPropertyName("symbol")]
+	public Icon? Symbol { get; set; }
+
+	[JsonPropertyName("symbolSize")]
+	public NumberOrNumberArray? SymbolSize { get; set; }
+
+	/// <summary>
+	/// Default '#ddd'
+	/// </summary>
+	[JsonPropertyName("color")]
+	public Color? Color { get; set; }
+
+	[JsonPropertyName("colorAlpha")]
     public double? ColorAlpha { get; set; }
+
+	[JsonPropertyName("opacity")]
+	public double? Opacity { get; set; }
+
+	/// <summary>
+	/// Lightness in HSL. Range [0, 1]
+	/// </summary>
+	[JsonPropertyName("colorLightness")]
+	public double? ColorLightness { get; set; }
+
+	/// <summary>
+	/// Saturation in HSL. Range [0, 1]
+	/// </summary>
+	[JsonPropertyName("colorSaturation")]
+	public double? ColorSaturation { get; set; }
+
+	/// <summary>
+	/// Hue in HSL. Range [0, 360]
+	/// </summary>
+	[JsonPropertyName("colorHue")]
+	public double? ColorHue { get; set; }
 }
